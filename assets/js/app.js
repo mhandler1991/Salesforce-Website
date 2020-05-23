d90f1a24dc794a023a9cc933c742ad703a7afc33
mhandler1991 <mhandler1991@gmail.com>
2020-05-23 16:41:31 -0700

    fix array capture for active tab

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ function tabUpdate() {
     // Get Current Active Tab
     const activeTab = document.getElementsByClassName('slds-is-active');
     console.log('Active Tabs Length: ', activeTab.length);
-    console.log('Active Tab: ', activeTab)[0];
+    console.log('Active Tab: ', activeTab[0]);
 
     // Remove Class
     console.log('Removing Active Class');
