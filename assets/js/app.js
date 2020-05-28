8478aace8e18096c7423ce683e0396dd4ba3eac6
mhandler1991 <mhandler1991@gmail.com>
2020-05-27 19:07:59 -0700

    test generate timeline

--- assets/js/app.js
+++ assets/js/app.js
@@ -98,71 +98,71 @@ function tabUpdate() {
 // *******************************
 // CHART JS - SKILLS CHART
 // *******************************
-function myskill(){
-    var skillsChart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: options
-    });
+
+function myskill() {
+    let skillsChart = document.getElementById('skillsChart').getContext('2d');
+
+    let chart = new Chart(skillsChart, {
+        type: 'horizontalBar', // Chart Type...
+        data: {
+            labels: [
+                'HTML',
+                'CSS',
+                'Javascript',
+                'Node.js',
+                'SQL',
+                'Mongo DB',
+                'React',
+                'Visualforce',
+                'Lightning Components',
+            ],
+            datasets: [{
+                label: 'Title',
+                data: [99, 90, 75, 50, 75, 50, 25, 50, 60, ],
+                backgroundColor: '#0C8EFF',
+                borderWidth: 1,
+                hoverBackgroundColor: '#00396B',
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Dev Skills',
+                fontSize: 25
+            },
+            legend: {
+                display: false
+            },
+            layout: {
+                padding: {
+                    right: 50
+                }
+            },
+            tooltips: {
+                enabled: false
+            },
+            scales: {
+                xAxes: [{
+                    display: false,
+                    gridLines: {
+                        drawOnChartArea: false
+                    }
+                }],
+                yAxes: [{
+                    gridLines: {
+                        drawOnChartArea: false
+                    }
+                }]
+            }
+        },
+    })
 }
 
+function createTimeline(){
 
+    let timeline = document.createElement('li');
+    let text = document.createTextNode('text');
+    timeline.appendChild(text);
+    document.getElementById("timeline").appendChild(timeline);
 
-// function myskill() {
-//     let skillsChart = document.getElementById('skillsChart').getContext('2d');
-
-//     let chart = new Chart(skillsChart, {
-//         type: 'horizontalBar', // Chart Type...
-//         data: {
-//             labels: [
-//                 'HTML',
-//                 'CSS',
-//                 'Javascript',
-//                 'Node.js',
-//                 'SQL',
-//                 'Mongo DB',
-//                 'React',
-//                 'Visualforce',
-//                 'Lightning Components',
-//             ],
-//             datasets: [{
-//                 label: 'Title',
-//                 data: [99, 90, 75, 50, 75, 50, 25, 50, 60, ],
-//                 backgroundColor: '#0C8EFF',
-//                 borderWidth: 1,
-//                 hoverBackgroundColor: '#00396B',
-//             }]
-//         },
-//         options: {
-//             title: {
-//                 display: true,
-//                 text: 'Dev Skills',
-//                 fontSize: 25
-//             },
-//             legend: {
-//                 display: false
-//             },
-//             layout: {
-//                 padding: {
-//                     right: 50
-//                 }
-//             },
-//             tooltips: {
-//                 enabled: false
-//             },
-//             scales: {
-//                 xAxes: [{
-//                     display: false,
-//                     gridLines: {
-//                         drawOnChartArea: false
-//                     }
-//                 }],
-//                 yAxes: [{
-//                     gridLines: {
-//                         drawOnChartArea: false
-//                     }
-//                 }]
-//             }
-//         },
-//     })
-// }
+}
