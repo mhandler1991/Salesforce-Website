7dec41dcd8cd63259d0d39307fc85cec7d56d963
mhandler1991 <mhandler1991@gmail.com>
2021-11-23 15:14:22 -0800

    comment out define json variable in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,20 +33,20 @@ document.getElementById("description").innerHTML = detailData['description'];
 
 
 // Define JSON Variable
-var getJSON = function (url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function () {
-        var status = xhr.status;
-        if (status === 200) {
-            callback(null, xhr.response);
-        } else {
-            callback(status, xhr.response);
-        }
-    };
-    xhr.send();
-};
+// var getJSON = function (url, callback) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', url, true);
+//     xhr.responseType = 'json';
+//     xhr.onload = function () {
+//         var status = xhr.status;
+//         if (status === 200) {
+//             callback(null, xhr.response);
+//         } else {
+//             callback(status, xhr.response);
+//         }
+//     };
+//     xhr.send();
+// };
 
 // *******************************
 // Javascript Tab Handling
